cea8071f1198a70bcddcf5a284ee85ee82aa7abb
manasjhamj <jhamanas939@gmail.com>
2024-01-10 21:01:06 +0530

    add glass effect

--- index.js
+++ index.js
@@ -22,3 +22,17 @@ function theme() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+VanillaTilt.init(document.querySelectorAll(".glassmorphic-card"), {
+    max: 10,
+    speed: 200,
+    glare: true,
+    "max-glare": 0.5,
+});
+
+VanillaTilt.init(document.querySelectorAll(".glassmorphic-card-mobile"), {
+    max: 14,
+    speed: 200,
+    glare: true,
+    "max-glare": 0.4,
+});
